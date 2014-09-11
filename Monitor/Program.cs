075b7f9406a9046aaf052a20197f2bfe7610cfa0
Carl Schweitzer <carl@caschw.com>
2014-09-11 06:17:29 -0500

    Updated readme and added comment to program.cs

--- Monitor/Program.cs
+++ Monitor/Program.cs
@@ -6,7 +6,12 @@
         const int SC_MONITORPOWER = 0xF170;
         const int HWND_BROADCAST = 0xFFFF;
         const int MONITOR_POWER_OFF = 2;
-        
+
+        /// <summary>
+        /// Sends command to turn monitors off, will be able wiggle mouse or tab keyboard key to wake.
+        /// </summary>
+        /// <param name="args">Not used</param>
+        /// <note>The program immediately exits.</note>
         static void Main(string[] args)
         {
             SendMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, MONITOR_POWER_OFF);
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 Monitor
 =======
 
-Program that immediately turns your monitor off.
+Program that immediately turns your monitor(s) off. 
+
+
+Great simple app to place in task scheduler to automate your monitors, or create a shortcut to the executable to turn off on demand.
