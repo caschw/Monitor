86fa077a8dfa4d7682e78b63aac3e2a13434053a
Carl Schweitzer <carl@caschw.com>
2014-09-11 06:13:17 -0500

    Adding solution

---
+++ Monitor/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Monitor
+{
+    class Program
+    {
+        const int WM_SYSCOMMAND = 0x0112;
+        const int SC_MONITORPOWER = 0xF170;
+        const int HWND_BROADCAST = 0xFFFF;
+        const int MONITOR_POWER_OFF = 2;
+        
+        static void Main(string[] args)
+        {
+            SendMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, MONITOR_POWER_OFF);
+        }
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern int SendMessage(int hWnd, int hMsg, int wParam, int lParam);
+    }
+}
